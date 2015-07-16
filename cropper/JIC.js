0221a6c4f04a9f36285a974db8d2d6d3b677d561
Ray0427 <ray13242000@gmail.com>
2015-07-16 18:25:54 +0800

    cropper

--- cropper/JIC.js
+++ cropper/JIC.js
@@ -24,29 +24,33 @@
          * @return {Image} result_image_obj The compressed Image Object
          */
 
-         compress: function(source_img_obj, width, height, output_format){
+         compress: function(source_img_obj, width,height, output_format){
 
-           var mime_type = "image/jpeg";
-           if(typeof output_format !== "undefined" && output_format=="png"){
-            mime_type = "image/png";
-        }
+             var mime_type = "image/jpeg";
+             if(typeof output_format !== "undefined" && output_format=="png"){
+                mime_type = "image/png";
+            }
 
-        var cvs = document.createElement('canvas');
-        if(source_img_obj.naturalHeight/source_img_obj.naturalWidth*width>height){
-            cvs.height=height;
-            cvs.width=width;
-        }else{
-             // cvs.height = source_img_obj.naturalHeight*quality/100;
-             cvs.width=width;
-             cvs.height=source_img_obj.naturalHeight/source_img_obj.naturalWidth*width;
-         }
-         console.log(cvs.width);
-         var ctx = cvs.getContext("2d").drawImage(source_img_obj, 0, 0,width,source_img_obj.naturalHeight/source_img_obj.naturalWidth*width);
-         var newImageData = cvs.toDataURL(mime_type, 1.0);
-         var result_image_obj = new Image();
-         result_image_obj.src = newImageData;
-         return result_image_obj;
-     },
+
+            var cvs = document.createElement('canvas');
+            if(source_img_obj.naturalWidth>width){
+                cvs.height=height;
+                cvs.width=width;
+            }
+            else{
+               cvs.width=source_img_obj.naturalWidth;
+               cvs.height=source_img_obj.naturalHeight;
+           }
+           console.log(cvs);
+           var ctx = cvs.getContext("2d");
+           ctx.fillStyle = "rgba(255,255,255,1)";
+           ctx.fillRect(0,0,cvs.width,cvs.height);
+           ctx.drawImage(source_img_obj, 0, 0,cvs.width,cvs.height);
+           var newImageData = cvs.toDataURL(mime_type, 0.6);
+           var result_image_obj = new Image();
+           result_image_obj.src = newImageData;
+           return result_image_obj;
+       },
 
         /**
          * Receives an Image Object and upload it to the server via ajax
